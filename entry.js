71855d8d43aa8bf8b232de33ff41eb35d9ec45d3
chenheling <6710582@qq.com>
2016-10-08 14:29:27 +0800

    10.8

--- entry.js
+++ entry.js
@@ -1,14 +1,18 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-
+const content = require('./content.js');
 var Entry = React.createClass({
-	render() {
+	componentDidMount:function(){
+		alert(content);
+	},
+	render: function() {
 		return (
 			<div>
 				<h1>Hello, world!</h1>
 			</div>
 		);
 	}
+	 
 });
 ReactDOM.render(
   <Entry/>,
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,29 @@
+var webpack = require('webpack');
+var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 module.exports = {
-  entry: './entry.js',
-  output: {
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders:[
-      {
-        test: /\.js[x]?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader?presets[]=es2015&presets[]=react'
-      },
+    entry: './entry.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js[x]?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader?presets[]=es2015&presets[]=react'
+        }, ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+
+        new uglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
     ]
-  }
+
 };
