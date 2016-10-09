4b6b6f575352f685c49597210e85a9b9fb5d8f8d
chenheling <6710582@qq.com>
2016-10-09 11:04:04 +0800

    10.9

--- src/entry.js
+++ src/entry.js
@@ -7,12 +7,15 @@ require('./LoadMask');
 require("!style!css!less!../node_modules/bootstrap/less/bootstrap.less");
 require("!style!css!less!../less/index.less");
 require("!style!css!less!../less/loadmask.less");
+
+require('../node_modules/bootstrap/dist/js/bootstrap.min'); 
+
 var Entry = React.createClass({
 	btnEnterClick: function() {
 		var me = this;
 
 		$('#wrapper').loadingOverlay();
-
+		$('#element').popover('show')
 		//$('.test').loadingOverlay('remove');
 
 	},
@@ -25,6 +28,11 @@ var Entry = React.createClass({
 			<div className='test'>
 				<h1>Hello, world!</h1>
 				<input type="button" className='btn btn-success' value='button' onClick={me.btnEnterClick}/>
+				<br/>
+				<button type="button" className="btn btn-default btn-lg">
+				  <span className="glyphicon glyphicon-star"></span> Star
+				</button>
+				<button id='element' type="button" className="btn btn-lg btn-danger" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
 			</div>
 		);
 	}
