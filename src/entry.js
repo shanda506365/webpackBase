413045f859bce5c914f4ec615000daab47abfa16
chenheling <6710582@qq.com>
2016-10-09 10:40:18 +0800

    10.9 bootstarp

--- src/entry.js
+++ src/entry.js
@@ -1,10 +1,12 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const content = require('./content');
+
+var $ = require('jquery');
+require('./LoadMask'); 
+require("!style!css!less!../node_modules/bootstrap/less/bootstrap.less");
 require("!style!css!less!../less/index.less");
 require("!style!css!less!../less/loadmask.less");
-var $ = require('jquery');
-require('./LoadMask');
 var Entry = React.createClass({
 	btnEnterClick: function() {
 		var me = this;
@@ -22,7 +24,7 @@ var Entry = React.createClass({
 		return (
 			<div className='test'>
 				<h1>Hello, world!</h1>
-				<input type="button" className='button' value='button' onClick={me.btnEnterClick}/>
+				<input type="button" className='btn btn-success' value='button' onClick={me.btnEnterClick}/>
 			</div>
 		);
 	}
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,26 @@
 var webpack = require('webpack');
 var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 module.exports = {
-    entry: './src/entry.js',
+    entry:  ['./src/entry.js'],
     output: {
         filename: 'bundle.js'
     },
     module: {
         loaders: [{
-            test: /\.js[x]?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader?presets[]=es2015&presets[]=react'
-        }, {
-            test: /\.less$/,
-            loader: "style!css!less"
-        }, { test: /\.css$/, loader: 'style-loader!css-loader' }, {
-            test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
-            loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
-        }]
+                test: /\.js[x]?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader?presets[]=es2015&presets[]=react'
+            }, {
+                test: /\.less$/,
+                loader: "style!css!less"
+            }, {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }, {
+                test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
+                loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
+            }
+        ]
     },
     plugins: [
         new webpack.DefinePlugin({
